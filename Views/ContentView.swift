6f73c5b69fd50827b75d1d027245f18733e4756a
19v <jhc_98219@live.com>
2024-12-13 15:52:04 +0800

    repo updated at 1734076324

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -4,29 +4,57 @@ struct ContentView: View {
     @State private var selectedTab: Tabs = .addWaterMark
     
     var body: some View {
-//        TabView(selection: $selectedTab) {
-//            Tab("添加水印", systemImage: "photo.badge.plus", value: .addWaterMark) {
-//                NavigationStack {
-//                    AddTabView()
-//                }
-//                .navigationTitle(selectedTab.title)
-//            }
-//            Tab("移除水印", systemImage: "photo.on.rectangle.angled.fill", value: .removeWaterMark) {
-//                RemoveTabView()
-//            }
-//            Tab("设置", systemImage: "gearshape", value: .settings) {
-//                SettingsTabView()
-//            }
-//        }
-//        // TODO: 上面的代码暂时注释，其他Tab没做完就不显示出来了，先搞定基础的功能-添加水印
-//        NavigationStack {
-//            AddTabView()
-//        }
-//        .navigationTitle(CommonUtils.appName)
         NavigationStack {
-            EditPhotoPage()
+//            AddTabView()
+            VStack {
+                GeometryReader { geometry in
+//                    Text("Marker")
+//                        .font(.system(size: 48, weight: .medium))
+//                        .frame(maxWidth: geometry.size.width, maxHeight: geometry.size.height)
+//                        .foregroundStyle(.white)
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Marker")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundStyle(.white)
+                        Text("创建带有水印的照片")
+                            .font(.subheadline)
+                            .foregroundStyle(Color(hex: "#E2E3E5"))
+                    }
+                    .padding(.top, 42) // 上方留一些间距
+                    .padding(.horizontal, 42) // 左右留间距
+                    .frame(maxWidth: geometry.size.width, maxHeight: geometry.size.height)
+                }
+                .padding(.top, CommonUtils.safeTopInset)
+                
+                VStack(spacing: 28) {
+                    CapsuleButton(icon: "photo.fill", title: "选择照片") {
+                        print("Button tapped!")
+                    }
+                    
+                    CapsuleButton(icon: "camera.fill", title: "拍摄照片") {
+                        print("Button tapped!")
+                    }
+                    
+                    HStack {
+                        Spacer()
+                        TextButton(icon: "gearshape.fill", title: "设置") {
+                            print("Button tapped!")
+                        }
+                        Spacer()
+                        TextButton(icon: "quote.bubble.fill", title: "反馈") {
+                            print("Button tapped!")
+                        }
+                        Spacer()
+                    }
+                }
+                .padding(EdgeInsets(top: 10, leading: 32, bottom: CommonUtils.safeBottomInset + 16, trailing: 32))
+            }
+            .background(
+                MeshGradientView()
+            )
         }
-        .navigationTitle(CommonUtils.appName)
     }
 }
 
