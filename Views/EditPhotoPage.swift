ede8ed2ab7c2b8b921d230325d5b5b424377c210
19v <jhc_98219@live.com>
2024-12-18 15:56:41 +0800

    repo updated at 1734508601

--- Views/EditPhotoPage.swift
+++ Views/EditPhotoPage.swift
@@ -14,24 +14,17 @@ struct EditPhotoPage: View {
     @State private var displayTime = false // 显示时间的开关
     @State private var displayCoordinate = false // 显示经纬度的开关
     
-    // 控制图片显示的参数
-    @State private var scale: CGFloat = 1.0 // 控制缩放比例
-    @State private var lastScale: CGFloat = 1.0 // 保存上一次的缩放比例
-    @State private var offset: CGSize = .zero // 偏移量
-    @State private var lastOffset: CGSize = .zero // 上一次偏移量
-    
     @State private var isShowingListView = false
     
     var body: some View {
         VStack {
-            // MARK: 中间部分图片
             GeometryReader { geometry in
                 switch viewModel.imageState {
                 case .empty:
                     VStack {
                         Image(systemName: "questionmark.circle.dashed")
                             .scaledToFit()
-                            .font(.system(size: 42))
+                            .font(.system(size: 40))
                             .foregroundStyle(.gray)
                         Text("图片未加载")
                             .foregroundStyle(.gray)
@@ -42,82 +35,7 @@ struct EditPhotoPage: View {
                     ProgressView()
                         .frame(width: geometry.size.width, height: geometry.size.height)
                 case .success(let image):
-                    ZStack {
-                        Color.white
-                            .opacity(0.01)
-                            .frame(width: geometry.size.width, height: geometry.size.height)
-                            .gesture(
-                                // 双击
-                                TapGesture(count: 2)
-                                    .onEnded {
-                                        withAnimation {
-                                            if offset != .zero {
-                                                offset = .zero
-                                                lastOffset = .zero
-                                            } else {
-                                                scale = scale == 1.0 ? 2.0 : 1.0
-                                            }
-                                        }
-                                    }
-                            )
-                        
-                        image
-                            .resizable()
-                            .scaledToFit()
-                            .scaleEffect(scale) // 缩放
-                            .offset(offset) // 偏移
-                            .gesture(
-                                // 拖拽
-                                DragGesture()
-                                    .onChanged { value in
-                                        offset = CGSize(
-                                            width: lastOffset.width + value.translation.width,
-                                            height: lastOffset.height + value.translation.height
-                                        )
-                                    }
-                                    .onEnded { _ in
-                                        lastOffset = offset
-                                    }
-                                    .simultaneously(
-                                        with: MagnificationGesture()
-                                            .onChanged { value in
-                                                scale = lastScale * value
-                                            }
-                                            .onEnded { _ in
-                                                lastScale = scale
-                                            }
-                                    )
-                            )
-                            .gesture(
-                                // 双指放大
-                                MagnificationGesture()
-                                    .onChanged { value in
-                                        scale = lastScale * value // 动态更新缩放比例
-                                    }
-                                    .onEnded { _ in
-                                        lastScale = scale // 保存最终缩放比例
-                                    }
-                            )
-                            .gesture(
-                                // 双击
-                                TapGesture(count: 2)
-                                    .onEnded {
-                                        withAnimation {
-                                            if offset != .zero {
-                                                offset = .zero
-                                                lastOffset = .zero
-                                            } else {
-                                                scale = scale == 1.0 ? 2.0 : 1.0
-                                            }
-                                        }
-                                    }
-                            )
-                        //                        .aspectRatio(contentMode: .fill)
-                            .frame(width: geometry.size.width, height: geometry.size.height)
-                        //                            .draggable(image)
-                            .listRowInsets(EdgeInsets())
-                    }
-                    .clipped()
+                    PhotoDisplayView(geometry: geometry, image: image)
                 case .failure:
                     Image(systemName: "exclamationmark.triangle.fill")
                         .font(.system(size: 40))
@@ -126,7 +44,6 @@ struct EditPhotoPage: View {
             }
             .frame(maxHeight: .infinity) // 占据剩余空间
             
-            // MARK: 底部工具栏
             HStack{
                 CustomTabButton(iconName: "calendar", labelText: "日期") {
                     print("Home button tapped")
@@ -139,17 +56,6 @@ struct EditPhotoPage: View {
                     print("Profile button tapped")
                 }
             }
-//            .frame(height: 80)
-//            .overlay(
-//                VStack {
-//                    Rectangle()
-//                        .fill(Color.gray.opacity(0.5)) // 边框颜色
-//                        .frame(height: 1) // 边框高度
-//                        .frame(maxWidth: .infinity)
-//                    Spacer()
-//                },
-//                alignment: .top
-//            )
         }
         .background(Color(hex: "#282828"))
         .navigationBarBackButtonHidden(true)
@@ -160,7 +66,7 @@ struct EditPhotoPage: View {
                     viewModel.imageLoaded.toggle() // 设置为 false 以 pop 页面
                     onDisappearAction()
                 } label: {
-                    Image(systemName: "xmark.circle.fill")
+                    Image(systemName: "xmark.circle")
                 }
             }
             
@@ -169,7 +75,7 @@ struct EditPhotoPage: View {
                 Button {
                     print("pressed")
                 } label: {
-                    Image(systemName: "square.and.arrow.down.fill")
+                    Image(systemName: "square.and.arrow.down")
                 }
             }
         }
@@ -183,6 +89,90 @@ struct EditPhotoPage: View {
     }
 }
 
-#Preview {
-    EditPhotoPage(viewModel: PhotoModel()) {}
+struct PhotoDisplayView: View {
+    let geometry: GeometryProxy
+    let image: Image
+    
+    // 控制图片显示的参数
+    @State private var scale: CGFloat = 1.0 // 控制缩放比例
+    @State private var lastScale: CGFloat = 1.0 // 保存上一次的缩放比例
+    @State private var offset: CGSize = .zero // 偏移量
+    @State private var lastOffset: CGSize = .zero // 上一次偏移量
+    
+    var body: some View {
+        ZStack {
+            Color.white
+                .opacity(0.01)
+                .frame(width: geometry.size.width, height: geometry.size.height)
+                .gesture(
+                    // 双击
+                    TapGesture(count: 2)
+                        .onEnded {
+                            withAnimation {
+                                if offset != .zero {
+                                    offset = .zero
+                                    lastOffset = .zero
+                                }
+                            }
+                    }
+                )
+            
+            image
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(scale) // 缩放
+                .offset(offset) // 偏移
+                .gesture(
+                    // 拖拽
+                    DragGesture()
+                        .onChanged { value in
+                            offset = CGSize(
+                                width: lastOffset.width + value.translation.width,
+                                height: lastOffset.height + value.translation.height
+                            )
+                        }
+                        .onEnded { _ in
+                            lastOffset = offset
+                        }
+                        .simultaneously(
+                            with: MagnificationGesture()
+                                .onChanged { value in
+                                    scale = lastScale * value
+                                }
+                                .onEnded { _ in
+                                    lastScale = scale
+                                }
+                        )
+                )
+                .gesture(
+                    // 双指放大
+                    MagnificationGesture()
+                        .onChanged { value in
+                            scale = lastScale * value // 动态更新缩放比例
+                        }
+                        .onEnded { _ in
+                            lastScale = scale // 保存最终缩放比例
+                        }
+                )
+                .gesture(
+                    // 双击
+                    TapGesture(count: 2)
+                        .onEnded {
+                            withAnimation {
+                                if offset != .zero {
+                                    offset = .zero
+                                    lastOffset = .zero
+                                } else {
+                                    scale = scale == 1.0 ? 2.0 : 1.0
+                                }
+                            }
+                        }
+                )
+//                .aspectRatio(contentMode: .fill)
+                .frame(width: geometry.size.width, height: geometry.size.height)
+//                .draggable(image)
+                .listRowInsets(EdgeInsets())
+        }
+        .clipped()
+    }
 }
