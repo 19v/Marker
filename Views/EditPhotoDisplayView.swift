671c55571d56566853849c96636d9687315b73ec
19v <jhc_98219@live.com>
2025-01-10 17:12:17 +0800

    adjust code

--- Views/EditPhotoDisplayView.swift
+++ Views/EditPhotoDisplayView.swift
@@ -17,58 +17,23 @@ struct EditPhotoDisplayView: View {
     
     var body: some View {
         ZStack {
-            // 背景（透明，仅用于检测手势）
-            Color.white
-                .opacity(0)
-                .contentShape(Rectangle())
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .gesture(
-                    // 双击
-                    TapGesture(count: 2)
-                        .onEnded {
-                            withAnimation {
-                                if offset != .zero {
-                                    offset = .zero
-                                    lastOffset = .zero
-                                }
-                            }
-                        }
-                )
-                .gesture(
-                    LongPressGesture(minimumDuration: 1)
-                        .onChanged { _ in
-                            isWatermarkDisplayed = false
-                        }
-                        .onEnded { _ in
-                            isWatermarkDisplayed = true
-                        }
-                )
-            
-            // 图片和水印
-            VStack(spacing: 0) {
-                image
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxWidth: .infinity)
-                    .listRowInsets(EdgeInsets())
-                
-                if let watermark,
-                   isWatermarkDisplayed {
-                    Image(uiImage: watermark)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(maxWidth: .infinity)
-                }
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity) // 占据剩余空间
-            .padding(.horizontal, 20)
-            .shadow(
-                color: colorScheme == .dark ? Color.white.opacity(0.1) : Color.black.opacity(0.2),
-                radius: colorScheme == .dark ? 20 : 10,
-                x: 0, y: 0
-            )
-            .scaleEffect(scale) // 缩放
-            .offset(offset) // 偏移
+            backgroundView
+            imageWithWatermark
+        }
+        .background(
+            colorScheme == .light
+            ? Color(hex: 0xF2F3F5)
+            : Color(hex: 0x101010)
+        )
+        .clipped()
+    }
+    
+    // 背景（透明，仅用于检测手势）
+    @ViewBuilder var backgroundView: some View {
+        Color.white
+            .opacity(0)
+            .contentShape(Rectangle())
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
             .gesture(
                 // 双击
                 TapGesture(count: 2)
@@ -77,50 +42,92 @@ struct EditPhotoDisplayView: View {
                             if offset != .zero {
                                 offset = .zero
                                 lastOffset = .zero
-                            } else {
-                                scale = scale == EditPhotoDisplayView.defaultScale ? 2.0 : EditPhotoDisplayView.defaultScale
                             }
                         }
                     }
             )
             .gesture(
-                // 拖拽
-                DragGesture()
-                    .onChanged { value in
-                        offset = CGSize(
-                            width: lastOffset.width + value.translation.width,
-                            height: lastOffset.height + value.translation.height
-                        )
+                LongPressGesture(minimumDuration: 1)
+                    .onChanged { _ in
+                        isWatermarkDisplayed = false
                     }
                     .onEnded { _ in
-                        lastOffset = offset
-                    }
-                    .simultaneously(
-                        with: MagnificationGesture()
-                            .onChanged { value in
-                                scale = lastScale * value
-                            }
-                            .onEnded { _ in
-                                lastScale = scale
-                            }
-                    )
-            )
-            .gesture(
-                // 双指放大
-                MagnificationGesture()
-                    .onChanged { value in
-                        scale = lastScale * value // 动态更新缩放比例
-                    }
-                    .onEnded { _ in
-                        lastScale = scale // 保存最终缩放比例
+                        isWatermarkDisplayed = true
                     }
             )
+    }
+    
+    // 图片和水印
+    @ViewBuilder var imageWithWatermark: some View {
+        VStack(spacing: 0) {
+            image
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity)
+                .listRowInsets(EdgeInsets())
+            
+            if let watermark,
+               isWatermarkDisplayed {
+                Image(uiImage: watermark)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity)
+            }
         }
-        .background(
-            colorScheme == .light
-            ? Color(hex: 0xF2F3F5)
-            : Color(hex: 0x101010)
+        .frame(maxWidth: .infinity, maxHeight: .infinity) // 占据剩余空间
+        .padding(.horizontal, 20)
+        .shadow(
+            color: colorScheme == .dark ? Color.white.opacity(0.1) : Color.black.opacity(0.2),
+            radius: colorScheme == .dark ? 20 : 10,
+            x: 0, y: 0
         )
-        .clipped()
+        .scaleEffect(scale) // 缩放
+        .offset(offset) // 偏移
+        .gesture(
+            // 双击
+            TapGesture(count: 2)
+                .onEnded {
+                    withAnimation {
+                        if offset != .zero {
+                            offset = .zero
+                            lastOffset = .zero
+                        } else {
+                            scale = scale == EditPhotoDisplayView.defaultScale ? 2.0 : EditPhotoDisplayView.defaultScale
+                        }
+                    }
+                }
+        )
+        .gesture(
+            // 拖拽
+            DragGesture()
+                .onChanged { value in
+                    offset = CGSize(
+                        width: lastOffset.width + value.translation.width,
+                        height: lastOffset.height + value.translation.height
+                    )
+                }
+                .onEnded { _ in
+                    lastOffset = offset
+                }
+                .simultaneously(
+                    with: MagnificationGesture()
+                        .onChanged { value in
+                            scale = lastScale * value
+                        }
+                        .onEnded { _ in
+                            lastScale = scale
+                        }
+                )
+        )
+        .gesture(
+            // 双指放大
+            MagnificationGesture()
+                .onChanged { value in
+                    scale = lastScale * value // 动态更新缩放比例
+                }
+                .onEnded { _ in
+                    lastScale = scale // 保存最终缩放比例
+                }
+        )
     }
 }
