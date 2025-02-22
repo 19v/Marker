93e60178ab485d3d2f1b4137a3bca0e66b6a5c6a
19v <jhc_98219@live.com>
2025-02-22 20:33:55 +0800

    增加双指捏合手势

--- Views/EditPhotoDisplayView.swift
+++ Views/EditPhotoDisplayView.swift
@@ -13,126 +13,142 @@ struct EditPhotoDisplayView: View {
     @State private var lastOffset: CGSize = .zero // 上一次偏移量
     
     @State private var anchorPoint: UnitPoint = .center // 记录双击的坐标（相对于视图）
+
+    @State private var contentSize: CGSize = .zero
     
     @Binding var isDisplayWatermark: Bool
     
     var body: some View {
-        ZStack {
-            // 背景（透明，仅用于检测手势）
-            Color.white
-                .opacity(0)
-                .contentShape(Rectangle())
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .onTapGesture {
-                    // 单击
-                    isDisplayWatermark = true
-                }
-                .gesture(
-                    // 双击
-                    TapGesture(count: 2)
-                        .onEnded {
-                            withAnimation {
-                                if offset != .zero {
-                                    offset = .zero
-                                    lastOffset = .zero
-                                }
-                                if scale != 1.0 {
-                                    scale = 1.0
-                                    lastScale = scale
-                                }
-                                if anchorPoint != .center {
-                                    anchorPoint = .center
-                                }
-                            }
-                        }
-                )
+        VStack(spacing: 0) {
+            Image(uiImage: viewModel.uiImage)
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity)
+                .listRowInsets(EdgeInsets())
             
-            // 图片和水印
-            VStack(spacing: 0) {
-                Image(uiImage: viewModel.uiImage)
+            if isDisplayWatermark {
+                Image(uiImage: viewModel.watermarkImage)
                     .resizable()
                     .scaledToFit()
                     .frame(maxWidth: .infinity)
-                    .listRowInsets(EdgeInsets())
-                
-                if isDisplayWatermark {
-                    Image(uiImage: viewModel.watermarkImage)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(maxWidth: .infinity)
-                }
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity) // 占据剩余空间
-            .padding(.horizontal, 20)
-            .shadow(
-                color: colorScheme == .dark ? Color.gray.opacity(0.1) : Color.black.opacity(0.2),
-                radius: colorScheme == .dark ? 12 : 10,
-                x: 0, y: 0
-            )
-            .offset(offset)
-            .scaleEffect(scale, anchor: anchorPoint)
-            // 双击放大、缩小和恢复原位
-            .onTapGesture(count: 2) { location in
-                if scale == 1.0 {
-                    // 计算缩放中心点
-                    let screenWidth = UIScreen.main.bounds.width
-                    let screenHeight = UIScreen.main.bounds.height
-                    anchorPoint = UnitPoint(x: location.x / screenWidth, y: location.y / screenHeight)
-                }
-                withAnimation {
-                    // 如果不在原位，优先恢复原位，并恢复原本大小
-                    if offset != .zero {
-                        offset = .zero
-                        lastOffset = .zero
-                        if scale != 1.0 {
-                            scale = 1.0
-                            lastScale = scale
-                            anchorPoint = .center
-                        }
-                    } else {
-                        if scale == 1.0 {
-                            scale = 2.0
-                        } else {
-                            anchorPoint = .center
-                            scale = 1.0
-                        }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity) // 占据剩余空间
+        .padding(.horizontal, 20)
+        .shadow(
+            color: colorScheme == .dark ? Color.gray.opacity(0.1) : Color.black.opacity(0.2),
+            radius: colorScheme == .dark ? 12 : 10,
+            x: 0, y: 0
+        )
+        .offset(offset)
+        .scaleEffect(scale, anchor: anchorPoint)
+        // 双击
+        .onTapGesture(count: 2) { location in
+            if scale == 1.0 {
+                // 计算缩放中心点
+                anchorPoint = UnitPoint(x: location.x / contentSize.width, y: location.y / contentSize.height)
+            }
+            withAnimation {
+                // 如果不在原位，优先恢复原位，并恢复原本大小
+                if offset != .zero {
+                    offset = .zero
+                    lastOffset = .zero
+                    if scale != 1.0 {
+                        scale = 1.0
                         lastScale = scale
+                        anchorPoint = .center
                     }
+                } else {
+                    if scale == 1.0 {
+                        scale = 2.0
+                    } else {
+                        anchorPoint = .center
+                        scale = 1.0
+                    }
+                    lastScale = scale
                 }
             }
-            // 拖拽
-            .gesture(
-                DragGesture()
-                    .onChanged { value in
+        }
+        // 拖拽
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    offset = CGSize(
+                        width: lastOffset.width + value.translation.width / scale,
+                        height: lastOffset.height + value.translation.height / scale
+                    )
+                }
+                .onEnded { value in
+                    lastOffset = offset
+                }
+        )
+        // 双指手势
+        .gesture(magnificationGesture)
+        .background(
+            GeometryReader { proxy in
+                // 背景（可用于检测手势）
+                Color(hex: colorScheme == .light ? 0xF2F3F5 : 0x101010)
+                    .opacity(0)
+                    .contentShape(Rectangle())
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .onTapGesture(count: 2) {
+                        // 双击
+                        withAnimation {
+                            if offset != .zero {
+                                offset = .zero
+                                lastOffset = .zero
+                            }
+                            if scale != 1.0 {
+                                scale = 1.0
+                                lastScale = scale
+                            }
+                            if anchorPoint != .center {
+                                anchorPoint = .center
+                            }
+                        }
+                    }
+                    .onAppear {
+                        // 设计上这张图的背景覆盖全屏，应该和 UIScreen.main.bounds 一致
+                        contentSize = proxy.size
+                    }
+            }
+        )
+    }
+    
+    // 双指手势
+    private var magnificationGesture: some Gesture {
+        MagnifyGesture(minimumScaleDelta: 0)
+            .onChanged { value in
+                withAnimation(.interactiveSpring) {
+                    if value.magnification > 1 {
+                        // 放大
+                        scale = lastScale + (value.magnification - 1.0)
+                        
+                        let newX = value.startLocation.x * (value.startAnchor.x - anchorPoint.x) * (scale - lastScale)
+                        let newY = value.startLocation.y * (value.startAnchor.y - anchorPoint.y) * (scale - lastScale)
                         offset = CGSize(
-                            width: lastOffset.width + value.translation.width / scale,
-                            height: lastOffset.height + value.translation.height / scale
+                            width: lastOffset.width - newX,
+                            height: lastOffset.height - newY
+                        )
+                    } else if value.magnification < 1 {
+                        // 缩小
+                        scale = lastScale * value.magnification
+                        
+                        let newX = value.startLocation.x * (value.startAnchor.x - anchorPoint.x) * (scale - lastScale)
+                        let newY = value.startLocation.y * (value.startAnchor.y - anchorPoint.y) * (scale - lastScale)
+                        offset = CGSize(
+                            width: lastOffset.width + newX,
+                            height: lastOffset.height + newY
                         )
                     }
-                    .onEnded { value in
-                        lastOffset = offset
-                    }
-            )
-            // 双指放大
-            .gesture(
-                MagnificationGesture()
-                    .onChanged { value in
-                        withAnimation {
-                            scale = lastScale * value
-                        }
-                    }
-                    .onEnded { _ in
-                        lastScale = scale
-                    }
-            )
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(
-            colorScheme == .light
-            ? Color(hex: 0xF2F3F5)
-            : Color(hex: 0x101010)
-        )
-        .clipped()
+                }
+            }
+            .onEnded { _ in
+                withAnimation(.interactiveSpring) {
+                    lastOffset = offset
+                    lastScale = scale
+                }
+            }
     }
     
 }
